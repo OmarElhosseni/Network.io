91d9923edf14ce8c26bd84cb0e68712e6812ebb8
OmarElhosseni <omarelhosseni123@gmail.com>
2018-08-14 11:08:33 -0400

    some stuff

--- scripts/authenticate.js
+++ scripts/authenticate.js
@@ -1,18 +1,27 @@
-const button = document.querySelector("#auth");
-button.addEventListener('click', function(event) {
-  event.preventDefault();
+// const button = document.querySelector("#signInButton");
+// button.addEventListener('click', function(event) {
+//   event.preventDefault();
+//
+//   // IN.User.authorize(function() {
+//   //   console.log('logged in');
+//   //   grabUserData();
+//   // });
+//
+// });
 
-  IN.User.authorize(function() {
-    console.log('logged in');
-    grabUserData();
-  });
-});
+function onLoad() {
+  IN.Event.on(IN, "auth", grabUserData);
+  const pass = document.createElement('input');
+
+}
 
 function grabUserData() {
+  document.getElementsByClassName('hidden')[0].style.display = 'inline'
+  document.getElementsByClassName('hidden')[1].style.display = 'inline'
   IN.API.Profile('me').fields([
-    'first-name', 'last-name', // Add these to get the name
-    'industry', 'date-of-birth', 'educations:(id,school-name)',
-    'positions' // Add this one to get the job history
+    // 'first-name', 'last-name', // Add these to get the name
+    // 'industry', 'date-of-birth', 'educations:(id,school-name)',
+    // 'positions' // Add this one to get the job history
   ]).result(function(profiles) {
     console.log(profiles.values[0]);
   });
