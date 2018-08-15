9b67bbd97351e472e0212555ea67886c827e1967
OmarElhosseni <omarelhosseni123@gmail.com>
2018-08-15 14:34:51 -0400

    user authenticationa and information storage

--- scripts/authenticate.js
+++ scripts/authenticate.js
@@ -1,4 +1,20 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCJjZ-8B-jHFfGqflEo7t-WmSNn84_AyMk",
+  authDomain: "networkio-2ffd5.firebaseapp.com",
+  databaseURL: "https://networkio-2ffd5.firebaseio.com",
+  projectId: "networkio-2ffd5",
+  storageBucket: "networkio-2ffd5.appspot.com",
+  messagingSenderId: "546255366020"
+};
+firebase.initializeApp(config);
 
+const txtName = document.getElementById('name');
+const pfp = document.getElementById('pfp')
+const txtEmail = document.getElementById('email');
+const txtPass = document.getElementById('pass');
+const btnSignin = document.getElementById('signIn');
+const btnSignup = document.getElementById('signUp');
 
 function onLoad() {
   IN.Event.on(IN, "auth", grabUserData);
@@ -19,20 +35,38 @@ function grabUserData() {
   });
 }
 
+btnSignup.addEventListener('click', e => {//Sign up new account
+ const email = txtEmail.value;
+ const pass = txtPass.value;
+ const nameU = txtName.textContent;
+ const auth = firebase.auth();
+ const inbox = [];
+ const pfpU = pfp.src;
+
+ // writeUserData(email, name);
+ const promise = auth.createUserWithEmailAndPassword(email,pass);
+ promise.then(u => {
+   firebase.database().ref('users/' + auth.currentUser.uid).set({
+     EMAIL : email,
+     NAME : nameU,
+     INBOX : inbox,
+     PFP : pfpU,
+   });
+ });
+ promise.catch(function() { console.log(e.message)});
+})
+
 function updateWithInfo(profile) {
-  document.getElementById('pass').style.display = 'block'
-  document.getElementById('signIn').style.display = 'block'
-  document.getElementById('signUp').style.display = 'block'
-  const pfp = document.getElementById('pfp')
+  txtPass.style.display = 'block'
+  btnSignin.style.display = 'block'
+  btnSignup.style.display = 'block'
   pfp.style.display = 'block'
   pfp.style.width = '200px'
   pfp.style.height = 'auto'
   console.log(profile.pictureUrl)
   pfp.src = profile.pictureUrl;
-  const name = document.getElementById('name');
-  name.style.display = 'block';
-  name.innerHTML = profile.firstName + ' ' + profile.lastName;
-  const email = document.getElementById('email');
+  txtName.style.display = 'block';
+  txtName.textContent = profile.firstName + ' ' + profile.lastName;
   email.style.display = 'block';
   email.value = profile.emailAddress;
 }
