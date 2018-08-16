3efde81a838efe8f9c64815e527bcc54d0e3307d
OmarElhosseni <omarelhosseni123@gmail.com>
2018-08-15 23:15:52 -0400

    adds divs from database

--- scripts/authenticate.js
+++ scripts/authenticate.js
@@ -9,25 +9,29 @@ var config = {
 };
 firebase.initializeApp(config);
 
+let profile = {};
+
 const txtName = document.getElementById('name');
 const pfp = document.getElementById('pfp')
 const txtEmail = document.getElementById('email');
 const txtPass = document.getElementById('pass');
 const btnSignin = document.getElementById('signIn');
 const btnSignup = document.getElementById('signUp');
+const btnLogout = document.getElementById('logOut');
 
 function onLoad() {
   IN.Event.on(IN, "auth", grabUserData);
 }
 
 function grabUserData() {
-  let profile = {};
+
   IN.API.Profile('me').fields([
     'first-name', 'last-name', // Add these to get the name
     'industry', 'date-of-birth', 'educations:(id,school-name)',
     'positions', // Add this one to get the job history
     'picture-url',//gets image
-    'email-address'
+    'email-address',//
+    'public-profile-url'
   ]).result(function(profiles) {
     console.log(profiles.values[0]);
     profile = profiles.values[0];
@@ -35,13 +39,26 @@ function grabUserData() {
   });
 }
 
+btnSignin.addEventListener('click', e => {//Log in users
+ const email = txtEmail.value;
+ const pass = txtPass.value;
+ const auth = firebase.auth();
+
+ const promise = auth.signInWithEmailAndPassword(email,pass);
+ promise.catch(e => console.log(e.message));
+
+ revealLogOut();
+})
+
 btnSignup.addEventListener('click', e => {//Sign up new account
  const email = txtEmail.value;
  const pass = txtPass.value;
  const nameU = txtName.textContent;
  const auth = firebase.auth();
- const inbox = [];
+ const inbox = [''];
  const pfpU = pfp.src;
+ const account = profile.publicProfileUrl;
+ const positions = profile.positions;
 
  // writeUserData(email, name);
  const promise = auth.createUserWithEmailAndPassword(email,pass);
@@ -49,11 +66,19 @@ btnSignup.addEventListener('click', e => {//Sign up new account
    firebase.database().ref('users/' + auth.currentUser.uid).set({
      EMAIL : email,
      NAME : nameU,
-     INBOX : inbox,
      PFP : pfpU,
+     ACOUNT: account,
+     INBOX : inbox,
+     POSITIONS : positions
    });
  });
  promise.catch(function() { console.log(e.message)});
+ revealLogOut();
+})
+
+btnLogout.addEventListener('click', e => {//Logs out user
+ firebase.auth().signOut();
+ btnLogout.style.display = 'none';
 })
 
 function updateWithInfo(profile) {
@@ -69,4 +94,8 @@ function updateWithInfo(profile) {
   txtName.textContent = profile.firstName + ' ' + profile.lastName;
   email.style.display = 'block';
   email.value = profile.emailAddress;
+}
+
+function revealLogOut() {
+  btnLogout.style.display = 'block';
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,58 @@
+var config = {
+  apiKey: "AIzaSyCJjZ-8B-jHFfGqflEo7t-WmSNn84_AyMk",
+  authDomain: "networkio-2ffd5.firebaseapp.com",
+  databaseURL: "https://networkio-2ffd5.firebaseio.com",
+  projectId: "networkio-2ffd5",
+  storageBucket: "networkio-2ffd5.appspot.com",
+  messagingSenderId: "546255366020"
+};
+firebase.initializeApp(config);
+
 const nearby = document.getElementById("nearby");
 const requests = document.getElementById("requests");
-console.log(nearby, requests);
+const database = firebase.database();
+const nearbyContainer = document.getElementById("nearbyContainer");
+const requestsContainer = document.getElementById("requestsContainer");
 
+updateWithProfiles()
+
+function updateWithProfiles() {
+  var leadsRef = database.ref('users');
+  leadsRef.on('value', function(snapshot) {
+    nearbyContainer.innerHTML = '';
+      snapshot.forEach(function(childSnapshot) {
+        var person = childSnapshot.val();
+        updateWithPerson(person);
+      });
+  });
+}
+
+function updateWithPerson(person) {
+  const newProfile = document.createElement('div');
+  const pfpN = document.createElement('IMG');
+  pfpN.src = person.PFP;
+  newProfile.appendChild(pfpN);
+  const info = document.createElement('P');
+  const br = document.createElement('br');
+  console.log(person);
+  info.innerText += person.NAME;
+  info.appendChild(br);
+  info.innerText += person.EMAIL;
+  info.appendChild(br);
+  if(person.POSITIONS.values) {
+    info.innerText += person.POSITIONS.values[0].title + ' at ' + person.POSITIONS.values[0].company.name;
+  }
+
+  nearbyContainer.appendChild(newProfile);
+  nearbyContainer.appendChild(info);
+}
 
 nearby.addEventListener("click", function() {
-    console.log
-    document.querySelector("#requestsContainer").style.display = "none";
+    updateWithProfiles()
     document.querySelector("#nearbyContainer").style.display = "block";
+    document.querySelector("#requestsContainer").style.display = "none";
 });
 requests.addEventListener("click", function() {
     document.querySelector("#nearbyContainer").style.display = "none";
     document.querySelector("#requestsContainer").style.display = "block";
 });
-
